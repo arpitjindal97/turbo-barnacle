1248efb6f80f0af87b891f9dd0383190ad43c358
Hallcoder <hallcoder25@gmail.com>
2022-08-22 11:21:01 +0200

    changed the api and search query structure

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,25 @@ import { useEffect, useState } from 'react'
 import Loader from '../components/Loader'
 import styles from '../styles/Home.module.css'
 import Checkbox from '../components/Checkbox'
+import { search } from '../lib/Requests'
 
 export default function Home() {
   const [isLoading,setLoading] = useState(true);
+  const [value,setValue] = useState('');
   useEffect(() => {
         setTimeout(() => {
        setLoading(false);
         },3000)
   },[])
+
+  const handleChange = (e) => {
+     setValue(e.target.value);
+     return;
+  }
+  const handleSearch = () => {
+    search(value);
+    return;
+  }
   return (
    <div className='bg-[#272325] min-h-screen flex flex-col'>
     <Head>
@@ -22,8 +33,8 @@ export default function Home() {
         <div style={isLoading ? {display:'none'}:{display:'flex'}} className='flex items-center flex-col mt-[10%]  min-h-[40vh] w-full'>
           <h1 className='text-4xl text-center w-11/12 m-6 font-bold text-white'>Welcome to <strong className='text-blue-900'>Turbo Barnacle</strong> , The Search Engine!</h1>
           <div className='w-5/12 flex justify-center items-center m-6 border rounded-md'>
-          <input className='w-11/12 h-12 text-white text-xl bg-inherit  indent-4 focus:outline-blue-600' placeholder='Search , The torrent world...'></input>
-          <MdSearch className='text-5xl text-white w-1/12 border-l cursor-pointer'/>
+          <input className='w-11/12 h-12 text-white text-xl bg-inherit  indent-4 focus:outline-blue-600' placeholder='Search , The torrent world...' onChange={handleChange} value={value}></input>
+          <MdSearch className='text-5xl text-white w-1/12 border-l cursor-pointer' onClick={handleSearch}/>
           </div>
         <div className='flex items-center justify-center m-auto w-10/12'>
           <Checkbox name='Movies'/>
